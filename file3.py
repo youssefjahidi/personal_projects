fb1cb092015c28a60880bd6c75217367e160afd3
youssefjahidi <youssef.jahidi@student.ecp.fr>
2017-11-16 15:17:10 +0800

    delete all functions

--- file3.py
+++ file3.py
@@ -2,28 +2,3 @@
 
 "F1"
 
-def f30():
-    pass
-
-def f31():
-    pass
-
-def f32():
-    pass
-
-def f33():
-    pass
-
-def f34():
-    pass
-
-def f35():
-    pass
-
-def f36():
-    pass
-
-def f37():
-    pass
-def f38():
-    pass
