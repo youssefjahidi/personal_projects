9a7190a451c6448dc70102917d40636f338641c6
youssefjahidi <youssef.jahidi@student.ecp.fr>
2017-11-16 09:42:46 +0800

    creation of file_tests_personnels

---
+++ fichier_tests_personnel.py
@@ -0,0 +1,22 @@
+#from string import *
+#from Classes_MY import *
+#from openpyxl import Workbook
+#from openpyxl import load_workbook
+#from string import *
+#import string
+#from datetime import datetime
+#import datetime as dt
+
+'''dict = {}
+l = ["aa", "bb", "cc"]
+
+
+for text in l:
+    dict[text] = ()
+    dict[text]. a'''
+
+dict = {"a": (1,2,3), "b": (5,6,7)}
+for val in dict.values():
+    print(val)
+    print(val[0])
+
